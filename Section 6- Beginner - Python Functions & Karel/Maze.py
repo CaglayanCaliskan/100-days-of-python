9b6e97de3a59aa98b5ac7647c68ad3d30452b914
Caglayan <caliskancaglayan@gmail>
2022-06-01 15:53:37 +0300

    Python Functions & Karel

---
+++ Section 6- Beginner - Python Functions & Karel/Maze.py
@@ -0,0 +1,23 @@
+# def turn_right():
+#     for n in range(3):
+#         turn_left()
+
+# def jump():
+#     if not wall_on_right():
+#         turn_right()
+#     else:
+#         turn_left()
+#     if not front_is_clear():
+#         turn_left()
+
+
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     move()
+
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
---
+++ Section 6- Beginner - Python Functions & Karel/main.py
@@ -0,0 +1,26 @@
+# https://docs.python.org/3/library/functions.html
+
+# def my_function(a):
+#     print('hello')
+#     print(a)
+
+
+# my_function("caglayan")
+
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+
+# for i in range(1, 7):
+#     print(i)
+
+# While true t t t...false stop
+# a = 5
+# while a < 12:
+#     print(a)
+#     a += 1
+
+# for n in range(3):
+#     print('hey')
+
+# I complated all senario on Reeborg's World !!!!
