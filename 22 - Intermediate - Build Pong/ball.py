17b05730682a8b3f0a2150e5c377b79ce170b878
Caglayan Caliskan <caliskancaglayan@gmail.com>
2022-11-23 18:31:25 +0300

    add the ball bouncing logic

--- 22 - Intermediate - Build Pong/ball.py
+++ 22 - Intermediate - Build Pong/ball.py
@@ -8,18 +8,13 @@ class Ball(Turtle):
         self.shape('circle')
         self.color('white')
         self.penup()
-        self.shapesize(stretch_wid=1, stretch_len=1)
-        self.goto(0, 0)
+        self.x_move = 10
+        self.y_move = 10
 
     def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
 
-    # def go_up(self):
-    #     new_y = self.ycor() + 20
-    #     self.goto(self.xcor(), new_y)
-
-    # def go_down(self):
-    #     new_y = self.ycor() - 20
-    #     self.goto(self.xcor(), new_y)
+    def bounce(self):
+        self.y_move *= -1
