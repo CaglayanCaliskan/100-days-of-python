dae7875efd3733665ae449938c006b4571edeb5e
Caglayan Caliskan <caliskancaglayan@gmail.com>
2022-11-24 14:30:45 +0300

    Create Player class

--- 23 - Intermediate - Turtle Crossing Game/main.py
+++ 23 - Intermediate - Turtle Crossing Game/main.py
@@ -1,5 +1,6 @@
 import time
 from turtle import Screen
+from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
@@ -7,8 +8,11 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
 
 
+screen.listen()
+screen.onkey(player.go_up, "Up")
 
 
 game_is_on = True
--- 23 - Intermediate - Turtle Crossing Game/player.py
+++ 23 - Intermediate - Turtle Crossing Game/player.py
@@ -6,3 +6,25 @@ MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape('turtle')
+        self.color('black')
+        self.penup()
+        self.left(90)
+        self.reset()
+
+    def go_up(self):
+        self.forward(MOVE_DISTANCE * 2)
+        self.crossing_control(self.ycor())
+
+    def reset(self):
+        self.penup()
+        self.goto(STARTING_POSITION)
+
+    def crossing_control(self, y_line):
+        if y_line > FINISH_LINE_Y:
+            # level up
+            #
+            self.reset()
