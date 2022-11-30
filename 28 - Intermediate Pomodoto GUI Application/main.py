628e8ebe9bdfd8997056b3751157048a72d067c5
Caglayan Caliskan <caliskancaglayan@gmail.com>
2022-11-30 16:54:14 +0300

    fix ui

--- 28 - Intermediate Pomodoto GUI Application/main.py
+++ 28 - Intermediate Pomodoto GUI Application/main.py
@@ -24,7 +24,18 @@ window.config(padx=100, pady=100, bg=YELLOW)
 canvas = Canvas(width=200, height=223, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.pack()
+canvas.grid(column=1, row=1)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+
+label_title = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 30))
+label_title.grid(column=1, row=0)
+
+start_button = Button(text="Start", width=6)
+start_button.grid(column=0, row=2)
+reset_button = Button(text="Reset", width=6)
+reset_button.grid(column=2, row=2)
+
+check_mark = Label(text="✔✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 24))
+check_mark.grid(column=1, row=3)
 
 window.mainloop()
