9264bda23d4e7d0e72ff6ac92abfecd68725ba4a
Caglayan Caliskan <caliskancaglayan@gmail.com>
2022-11-30 09:46:55 +0300

    Tkinter Label,Button and Entry

--- 27 - Intermediate Tkinter, args, kwargs and Creating Gui Programs/lesson1.py
+++ 27 - Intermediate Tkinter, args, kwargs and Creating Gui Programs/lesson1.py
@@ -10,7 +10,27 @@ window.minsize(width=500, height=300)
 
 my_label = tkinter.Label(text="Hello there label",
                          font=('Arial', 24, "italic"))
-my_label.pack(side="left")
+my_label.pack()
+
+my_label["text"] = "New text"  # I can change like this
+my_label.config(text="new text 2")  # or like this
+
+# Button
+
+
+def button_event():
+    print("clicked")
+    my_label.config(text="clicked")
+    my_label.config(text=input.get())
+
+
+button = tkinter.Button(text="Click me", command=button_event)
+button.pack()
+
+# Enrty
+
+input = tkinter.Entry(width=50)
+input.pack()
 
 
 window.mainloop()
