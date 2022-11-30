116b95f7df7385bf225bb2cc19564b2ccf36917b
Caglayan Caliskan <caliskancaglayan@gmail.com>
2022-11-30 13:37:56 +0300

    pack ,place and grid display system

--- 27 - Intermediate Tkinter, args, kwargs and Creating Gui Programs/lesson1.py
+++ 27 - Intermediate Tkinter, args, kwargs and Creating Gui Programs/lesson1.py
@@ -4,18 +4,7 @@ import tkinter
 window = tkinter.Tk()
 window.title("My first gui program")
 window.minsize(width=500, height=300)
-
-
-# Label
-
-my_label = tkinter.Label(text="Hello there label",
-                         font=('Arial', 24, "italic"))
-my_label.pack()
-
-my_label["text"] = "New text"  # I can change like this
-my_label.config(text="new text 2")  # or like this
-
-# Button
+window.config(padx=20, pady=20)
 
 
 def button_event():
@@ -24,13 +13,30 @@ def button_event():
     my_label.config(text=input.get())
 
 
+# Label
+
+my_label = tkinter.Label(text="Hello there label",
+                         font=('Arial', 24, "italic"))
+
+
+my_label["text"] = "New text"  # I can change like this
+my_label.config(text="new text 2")  # or like this
+
+my_label.grid(column=0, row=0)
+
+# Button
+
+
 button = tkinter.Button(text="Click me", command=button_event)
-button.pack()
+button.grid(column=1, row=1)
+
+new_button = tkinter.Button(text="new button")
+new_button.grid(column=3, row=2)
 
 # Enrty
 
-input = tkinter.Entry(width=50)
-input.pack()
+input = tkinter.Entry(width=10)
+input.grid(column=2, row=0)
 
 
 window.mainloop()
