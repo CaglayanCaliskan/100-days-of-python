83236a2f66b0011ae085b5a83c71ef0e93e8943f
Caglayan Caliskan <caliskancaglayan@gmail.com>
2022-12-01 14:57:23 +0300

    day 29 end

--- 29- Intermediate Password Application/main.py
+++ 29- Intermediate Password Application/main.py
@@ -1,18 +1,59 @@
+import random
 from tkinter import *
+from tkinter import messagebox
+import pyperclip
+
+printable = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+
+def generator():
+    password = ""
+    for _ in range(4):
+        list = [random.choice(printable) for i in range(4)]
+        block = "".join(list)
+        if _ == 3:
+            password += block
+        else:
+            password += block + "-"
+    entry_password.delete(0, END)
+    entry_password.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
-# ---------------------------- UI SETUP ------------------------------- #
 
+def save_password():
+    website = entry_website.get()
+    email = entry_email.get()
+    password = entry_password.get()
+
+    if len(website) < 1 or len(email) < 1:
+        messagebox.showerror(title=f"Oops",
+                             message="please fill blanks")
+    else:
+        is_ok = messagebox.askokcancel(
+            title="Comfirm", message=f"These are the derails entered: \nEmail: {email}\nPassword: {password}\nIs it ok to save? ")
+        if is_ok:
+            with open("passwords.txt", "a") as file:
+                file.write(f"{website} | {email} | {password}\n")
+                entry_email.delete(0, END)
+                entry_email.insert(0, "SAVED to File")
+                entry_website.delete(0, END)
+                entry_website.insert(0, "SAVED to File")
+
+
+# ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Generator")
-window.config(bg="white", padx=100, pady=100)
+window.config(bg="white", padx=40, pady=50)
 
 
-canvas = Canvas(width=200, height=189, bg="white", border=2)
-tomato_img = PhotoImage(file="logo.png")
-canvas.create_image(100, 94, image=tomato_img)
+canvas = Canvas(width=200, height=200, bg="white", highlightthickness=0)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0, columnspan=2)
 
 # Static titles
@@ -26,21 +67,23 @@ label_password = Label(text="Password:", bg="white", pady=10)
 label_password.grid(column=0, row=5)
 
 # Inputs
-entry_website = Entry(width=60, border=2)
+entry_website = Entry(border=2, width=50)
 entry_website.grid(column=1, row=3, columnspan=2)
+entry_website.focus()
 
-entry_email = Entry(width=60, border=2)
+entry_email = Entry(border=2, width=50)
 entry_email.grid(column=1, row=4, columnspan=2)
+entry_email.insert(0, "example@gmail.com")
 
-entry_password = Entry(border=2, width=40)
+entry_password = Entry(border=2, width=32)
 entry_password.grid(column=1, row=5)
 
 # Buttons
 button_generate = Button(text="Generate Password",
-                         border=1, bg="#FC2E20", fg="white")
+                         border=1, bg="#FC2E20", fg="white", command=generator)
 button_generate.grid(column=2, row=5)
 
-button_add = Button(text="Add", bg="#FD7F20", fg="white").grid(
+button_add = Button(text="Add", bg="#FD7F20", fg="white", command=save_password).grid(
     column=1, columnspan=2, row=6, sticky="we")
 
 window.mainloop()
