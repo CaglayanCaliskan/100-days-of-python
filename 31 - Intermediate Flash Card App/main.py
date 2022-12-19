8f7b9ec3fcf83d0f0866ec6cd650e43cdb21502c
Caglayan Caliskan <caliskancaglayan@gmail.com>
2022-12-19 22:17:06 +0300

    day 31 done

--- 31 - Intermediate Flash Card App/main.py
+++ 31 - Intermediate Flash Card App/main.py
@@ -1,3 +1,4 @@
+import time
 from tkinter import *
 import pandas as pd
 import random
@@ -18,12 +19,31 @@ def next_card():
     canvas.itemconfig(card_word, text=current_card["Deutch"], fill="black")
     canvas.itemconfig(card_background, image=card_front_img)
     flip_timer = window.after(3000, func=flip_card)
+    # timer()
 
 
 def flip_card():
     canvas.itemconfig(card_title, text="Türkçe", fill="white")
     canvas.itemconfig(card_word, text=current_card["Türkçe"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data = pd.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
+
+# def timer():
+#     count = 3
+#     canvas.itemconfig(counter, text=count)
+#     for t in range(count, 0, -1):
+#         print(count)
+#         count -= 1
+#         time.sleep(1)
+#         canvas.itemconfig(counter, text=count)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -42,6 +62,8 @@ card_title = canvas.create_text(
     400, 150, font=('Ariel', 40, 'italic'))
 card_word = canvas.create_text(
     400, 263, font=('Ariel', 60, 'bold'))
+counter = canvas.create_text(700, 50, font=(
+    'Ariel', 60, 'bold'), text="3", fill="green")
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=2)
 
@@ -52,7 +74,7 @@ unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="./images/right.png")
 known_button = Button(
-    image=check_image, highlightthickness=0, command=next_card)
+    image=check_image, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
 next_card()
