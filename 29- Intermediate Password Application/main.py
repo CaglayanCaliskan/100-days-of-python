28ffb491e2ea91ac347ab165dd7bc87b5a1a9981
Caglayan Caliskan <caliskancaglayan@gmail.com>
2022-12-19 13:48:22 +0300

    refactor password application Feature Search Button

--- 29- Intermediate Password Application/main.py
+++ 29- Intermediate Password Application/main.py
@@ -64,6 +64,28 @@ def save_password():
                 entry_website.delete(0, END)
                 entry_website.insert(0, "SAVED to File")
 
+# ---------------------------- FIND PASSWORD ------------------------------- #
+
+
+def find_password():
+    website = entry_website.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+
+    except FileNotFoundError:
+        messagebox.showerror(
+            title="404 Not Found", message=f"No Data File exist")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(
+                title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(
+                title="404 Not Found", message=f"Not found any password,Please try another input")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -87,8 +109,8 @@ label_password = Label(text="Password:", bg="white", pady=10)
 label_password.grid(column=0, row=5)
 
 # Inputs
-entry_website = Entry(border=2, width=50)
-entry_website.grid(column=1, row=3, columnspan=2)
+entry_website = Entry(border=2, width=32)
+entry_website.grid(column=1, row=3)
 entry_website.focus()
 
 entry_email = Entry(border=2, width=50)
@@ -99,11 +121,16 @@ entry_password = Entry(border=2, width=32)
 entry_password.grid(column=1, row=5)
 
 # Buttons
+button_search = Button(text="Search", border=1,
+                       bg='#FC2E20', fg="white", width=14, command=find_password)
+button_search.grid(column=2, row=3)
+
 button_generate = Button(text="Generate Password",
                          border=1, bg="#FC2E20", fg="white", command=generator)
 button_generate.grid(column=2, row=5)
 
 button_add = Button(text="Add", bg="#FD7F20", fg="white", command=save_password).grid(
     column=1, columnspan=2, row=6, sticky="we")
+
 
 window.mainloop()
