b7c6b38f7f02ff74e858c0e6774ec04240d8ee6b
Caglayan Caliskan <caliskancaglayan@gmail.com>
2023-01-02 20:34:34 +0300

    check if it will rain

--- 35 - Intermediate API,Keys,Auth+Send SMS/main.py
+++ 35 - Intermediate API,Keys,Auth+Send SMS/main.py
@@ -1,14 +1,26 @@
 import requests
-api_key = "API KEY FROM SITE"
+api_key = "5f4ed2e892ddd9ac2db91866cea160e3"
 web = "https://api.openweathermap.org/data/2.5/onecall"
 
 parameters = {
     "lat": 38.42,
     "lon": 27.14,
-    "exclude": "current",
+    "exclude": "daily,minutely,current",
     "appid": api_key
 }
 
 response = requests.get(web, params=parameters)
-data = response.json()
-print(data)
+
+weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
+
+will_rain = False
+
+
+for hour_data in weather_slice:
+    condition_code = (hour_data["weather"][0]["id"])
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("bring an umbrella")
