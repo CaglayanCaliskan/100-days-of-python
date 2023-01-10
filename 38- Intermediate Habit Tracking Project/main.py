d8ba457bac82b0f2c6935546a29886fcf90da2f0
Caglayan Caliskan <caliskancaglayan@gmail.com>
2023-01-11 01:09:36 +0300

    day 38 done
    new post requests for all converted exercises

--- 38- Intermediate Habit Tracking Project/main.py
+++ 38- Intermediate Habit Tracking Project/main.py
@@ -10,8 +10,13 @@ google_sheets = os.getenv('GOOGLESHEETS')
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
 today_date = datetime.today().strftime('%d/%m/%Y')
+current_time = datetime.now().strftime('%H:%M:%S')
+
 os.system('cls')
+
 my_exercise = input('What did you today?')
+# input example : ran 10km, swim 1km, cycling 45minutes
+
 query = {
     "query": my_exercise,
     "gender": "male",
@@ -24,6 +29,28 @@ headers = {
     "x-app-id": nut_id,
     "x-app-key": nut_api,
 }
+
 response = requests.post(url=exercise_endpoint, headers=headers, json=query)
 exercises_response_list = response.json()['exercises']
-print(exercises_response_list)
+
+
+for exercise in exercises_response_list:
+
+    body = {
+        "datum": {
+            "date": today_date,
+            "time": current_time,
+            "exercise": str(exercise['name']).capitalize(),
+            "duration": int(exercise['duration_min']),
+            "calories": int(exercise['nf_calories']),
+        }
+    }
+    requests.post(url=google_sheets, json=body)
+
+
+print('.')
+print('...')
+print('.....')
+print('........')
+print('..........')
+print('Thanks, Your info added to googlesheets')
