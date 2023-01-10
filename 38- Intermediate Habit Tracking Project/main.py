50f4bb28c831cacc4cfb97f6451fc8a2f820783b
Caglayan Caliskan <caliskancaglayan@gmail.com>
2023-01-11 00:55:14 +0300

    get exercises response data from api

--- 38- Intermediate Habit Tracking Project/main.py
+++ 38- Intermediate Habit Tracking Project/main.py
@@ -8,3 +8,22 @@ nut_api = os.getenv('NUTRITION_API_KEY')
 nut_id = os.getenv('NUTRITION_API_ID')
 google_sheets = os.getenv('GOOGLESHEETS')
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+today_date = datetime.today().strftime('%d/%m/%Y')
+os.system('cls')
+my_exercise = input('What did you today?')
+query = {
+    "query": my_exercise,
+    "gender": "male",
+    "weight_kg": 83.5,
+    "height_cm": 190,
+    "age": 55
+}
+
+headers = {
+    "x-app-id": nut_id,
+    "x-app-key": nut_api,
+}
+response = requests.post(url=exercise_endpoint, headers=headers, json=query)
+exercises_response_list = response.json()['exercises']
+print(exercises_response_list)
