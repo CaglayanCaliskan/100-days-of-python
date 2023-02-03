959fd6039cd6c8fdb0136f8f9fbf63d99888541a
Caglayan Caliskan <caliskancaglayan@gmail.com>
2023-02-03 15:09:53 +0300

    view movie list items

--- 64 - Advanced My Top10 movies website/Starting+Files+-+movie-project-start/main.py
+++ 64 - Advanced My Top10 movies website/Starting+Files+-+movie-project-start/main.py
@@ -10,10 +10,48 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
 
+db = SQLAlchemy()
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Movie.db"
+# initialize the app with the extension
+db.init_app(app)
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, unique=True, nullable=False)
+    year = db.Column(db.Integer, unique=True, nullable=False)
+    description = db.Column(db.String)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.String, nullable=False)
+    img_url = db.Column(db.String, nullable=False)
+
+
+# new_movie = Movie(
+#     title="Phone Booth",
+#     year=2002,
+#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#     rating=7.3,
+#     ranking=10,
+#     review="My favourite character was the caller.",
+#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+# )
+
+# with app.app_context():
+#     db.session.add(new_movie)
+#     db.session.commit()
+
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    myDb = db.session.execute(db.select(Movie)).scalars()
+    data = []
+    for item in myDb:
+        data.append(item)
+        print(item)
+
+    return render_template("index.html", data=data)
 
 
 if __name__ == '__main__':
